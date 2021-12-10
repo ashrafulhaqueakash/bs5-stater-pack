42dba2987b3dd7155c93abefbb14474402a257a2
sakib <ebnesakib2500@gmail.com>
2021-12-10 14:41:03 +0600

    Html and Css Complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,7 @@
 'use strict'
 
-if (jQuery(".hero-area__slider").length > 0) {
-    $(".hero-area__slider").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        centerMode: true,
-        centerPadding: 0,
-        focusOnSelect: true,
-        arrows: true,
-        fade: true,
-    });
-}
+$(".client-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+});
